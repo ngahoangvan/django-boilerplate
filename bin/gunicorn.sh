3fbc81604cf45178f792d15dda90c350410d6f52
Nga Hoang V <ngahv2222@gmail.com>
2022-08-10 14:44:36 +0700

    Setup nginx and bin script

--- bin/gunicorn.sh
+++ bin/gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python3 /app/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn app.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
+/usr/local/bin/gunicorn app.wsgi -w 4 -b 0.0.0.0:8000 --chdir=/app
--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -16,7 +16,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
